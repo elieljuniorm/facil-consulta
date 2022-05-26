256f54a9e1628278afb121b51b15fb4fc3e02250
Eliel Maia <elieljuniorm@gmail.com>
2022-05-25 21:24:06 -0300

    estilos finalizados, falta trocar a cor dos botoes

---
+++ src/components/Final.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="Final">
+        <div class="container">
+            <img src="https://cdn-icons-png.flaticon.com/512/4861/4861675.png" alt="médica" class="imgMedica">
+        </div>
+        <h1 class="titulo">Obrigado por se cadastrar</h1>
+        <h2 class="subtitulo">Em breve você receberá seus agendamentos de consulta no telefone cadastrado &#128516</h2>
+
+        <Botao rota="/revisao" label="OK" />
+    </div>
+</template>
+
+<script>
+import Botao from "@/components/Botao.vue"
+export default {
+    components: { Botao }
+}
+</script>
+
+<style scoped>
+.container{
+    padding-top: 1em;
+}
+.imgMedica {
+    height: 20em;
+}
+
+.titulo {
+    font-family: var(--fonte-padrao-conforta);
+    font-weight: bold;
+    color: var(--cor-letra-titulo);
+    padding-top: 0.5em;
+    text-align: center;
+}
+
+.subtitulo {
+    font-family: var(--fonte-padrao-open);
+    font-size: 1.3em;
+    font-weight: 600;
+    padding: 1em 0;
+    text-align: center;
+}
+</style>
--- src/views/Finalizado.vue
+++ src/views/Finalizado.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="finalizado">
     <BotaoSeta rota="/" label="<" />
-    Cadastro finalizado
+    <Final/>
   </div>
 </template>
 
 <script>
 import BotaoSeta from "@/components/BotaoSeta.vue";
+import Final from "@/components/Final.vue";
 export default {
-  components: { BotaoSeta },
+  components: { BotaoSeta, Final },
 };
 </script>
 <style scoped>
@@ -17,7 +18,7 @@ export default {
   border-top-right-radius: 40px;
   border-top-left-radius: 40px;
   width: 100%;
-  height: auto;
+  height: 100vh;
   margin: 4em 0 0 0;
   color: rgb(1, 24, 24);
   box-shadow: 5px 5px 10px rgb(0 0 0 / 57%);
