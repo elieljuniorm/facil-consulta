7cbd26c7176165985414b99776fef0a4a0946971
Eliel Maia <elieljuniorm@gmail.com>
2022-05-25 23:53:22 -0300

    retirando botao, erro

--- src/views/Finalizado.vue
+++ src/views/Finalizado.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="finalizado">
-    <BotaoSeta rota="/" label="<" />
     <Final/>
   </div>
 </template>
 
 <script>
-import BotaoSeta from "@/components/BotaoSeta.vue";
 import Final from "@/components/Final.vue";
 export default {
-  components: { BotaoSeta, Final },
+  components: { Final },
 };
 </script>
 <style scoped>
